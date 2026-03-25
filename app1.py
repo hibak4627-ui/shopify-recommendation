1d4e42c6285d3fddf83e73dac1b1e263e6fcf703
hibak4627-ui <hibak4627@gmail.com>
2026-03-25 10:45:59 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -9,7 +9,7 @@ import psycopg2
 import logging
 from flask import Flask, request, jsonify
 from datetime import datetime
-from flask_cors import CORS 
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={r"/events/*": {"origins": "*"}})
